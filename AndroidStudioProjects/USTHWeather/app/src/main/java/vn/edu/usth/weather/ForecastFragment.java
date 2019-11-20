7f86b964c9b49947b341958e9ecc3e7676fcace3
duoiuoicai <phamvietminhduc@gmail.com>
2019-11-20 14:15:06 +0700

    Whole folder

---
+++ AndroidStudioProjects/USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -0,0 +1,24 @@
+package vn.edu.usth.weather;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class ForecastFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = new View(getContext());
+        view.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+        return view;
+    }
+}
---
+++ AndroidStudioProjects/USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -0,0 +1,54 @@
+package vn.edu.usth.weather;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+
+public class WeatherActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+        setContentView(R.layout.activity_weather);
+        Log.i("log", "This is Create");
+        // Create a new Fragment to be placed in the activity layout
+        ForecastFragment firstFragment = new ForecastFragment();
+// Add the fragment to the 'container' FrameLayout
+        getSupportFragmentManager().beginTransaction().add(
+                R.id.container, firstFragment).commit();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("log","This is Start");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("log", "This is Stop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("log","This is Destroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("log", "This is Pause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("log", "This is Resume");
+    }
+}
